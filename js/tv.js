71548b0f0b4c7bfa8e89de54213e85df9970ea4e
pebrop <115961122+pebrop@users.noreply.github.com>
2024-06-20 17:39:33 +0530

    Add files via upload

---
+++ js/tv.js
@@ -0,0 +1,30 @@
+Status = "";
+objects = [];
+
+
+
+function preload() {
+
+    img = loadImage('dog_cat.jpg');
+}
+function setup() {
+    canvas = createCanvas(640, 420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+function modelLoaded() {
+    console.log("Model Loaded");
+    Status = true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results) {
+    if(error) {
+        console.log(error);
+    }
+    console.log(results);
+   
+}
+
